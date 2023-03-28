ace6871910b0a49a01b7aaa6d142d313fbfc69c4
reozmen <recepozmen41@gmail.com>
2023-03-28 16:43:14 +0300

    first day of javascript

---
+++ Day_01/variables.js
@@ -0,0 +1,24 @@
+let fisrtName = "Cumali"; //*string
+let lastName = "Kavaklı"; //*string
+let country = "Almanya"; //*string
+let age = 31; //*number
+let isMarried = false; //*boolean
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^/
+//^ Decalaring variables with number values
+//^ Javascript te ondalıklı sayılar ve tam sayılar number veri türündedir..
+
+const gravity = 9.81; //* number - earth gravity
+const boilingPoint = 100; //* number - water boiling point
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^/
+//^ Değişkenler 1 satırda virgül ile ayrılmış şekilde tanımalanabilirler..
+
+let name = "Cumali Kavaklı",
+  job = "Teacher",
+  live = "Spain";
+
+console.log(name, ",", job, ",", live);
+console.log("Gravity of earth is ", gravity);
+console.log(`Gravity of earth is ${gravity}`);
+console.log(`Boiling point of water is ${boilingPoint}`);
