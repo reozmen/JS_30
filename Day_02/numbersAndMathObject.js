7465567498eae82c0bdce1b2fe9d1591e541c13b
reozmen <recepozmen41@gmail.com>
2023-05-02 00:46:07 +0300

    numbers çalışıldı

---
+++ Day_02/numbersAndMathObject.js
@@ -0,0 +1,23 @@
+//^------------------------------------------------//
+//^ NUMBERS ------//
+//^------------------------------------------------//
+
+//*Numbers, tüm aritmetik işlemleri yapabilen tam sayılar ve ondalık değerlerdir.
+
+let age = 35;
+const gravity = 9.81; //~ Değişmeyen değerler için const kullanılmalıdır.. Yerçekimi sabit bir değerdir.
+let mass = 72; //~ mass in Kilogram
+const PI = 3.14; //~ Pİ sayısı sabit bir değerdir.
+
+//? More Examples
+const boilingPoint = 100; //~ Suyun kaynama noktası, sabit bir değerdir.
+
+const bodyTemp = 37; //~ Ortalama insan vücut sıcaklığı, sabit bir değerdir.
+
+//* Değişmeyen değerler için değişken adı büyük harflerle de yazılabilir. Bu, değişkenin sabit olduğunu gösterir.. Örnek: const PI = 3.14; const BOILING_POINT = 100; const BODY_TEMP = 37; ... Değişmeyen değerlerin değişken isimleri için bu gübü örneklerle de karşılaşabilirsiniz.
+
+console.log(age, gravity, mass, PI, boilingPoint, bodyTemp);
+
+//^------------------------------------------------//
+//^ MATH OBJECT ------//
+//^------------------------------------------------//
