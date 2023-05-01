32564fdd82dbbcf3cbebc3224572f6b6bffd3734
reozmen <recepozmen41@gmail.com>
2023-05-01 23:49:31 +0300

    data types çalışılıyor

--- Day_02/data_types.js
+++ Day_02/data_types.js
@@ -10,3 +10,34 @@ firstName[0] = "M";
 console.log(firstName); //* Ahmet
 
 //! Stringler değiştirilemez (immutable) veri türleridir. Bu yüzden firstName[0] = "M" işlemi gerçekleşmez. firstName değişkeni hala "Ahmet" olarak kalır.
+
+//***************************************************** */
+
+//* İlkel veri türleri, değerlerine göre karşılaştırılır.
+
+let number1 = 10;
+let number2 = 10;
+
+console.log(number1 == number2); //* true
+console.log(number1 === number2); //* true
+//? === veri türünü de kontrol eder.
+
+let js = "JavaScript";
+let py = "Python";
+
+console.log(js == py); //* false
+console.log(js === py); //* false
+
+let lightOn = true;
+let lightOff = false;
+
+console.log(lightOn == lightOff); //* false
+console.log(lightOn === lightOff); //* false
+
+//? === veri türünü de kontrol eder.
+
+let num1 = 10;
+let num2 = "10";
+
+console.log(num1 == num2); //* true
+console.log(num1 === num2); //* false (veri türü farklı olduğu için)
