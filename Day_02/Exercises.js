080cd98d4cf215fa625b7923c65d7574dbbdbdc3
reozmen <recepozmen41@gmail.com>
2023-05-03 22:55:12 +0300

    exercises 1 yapıldı

---
+++ Day_02/Exercises.js
@@ -0,0 +1,6 @@
+//^^ Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+
+//^^ Challenge adında bir değişken tanımlayın ve bunu '30 Days Of JavaScript' başlangıç ​​değerine atayın.
+
+let challenge = "30 Days Of JavaScript";
+console.log(challenge);
