bdde317a1f7320e030da85b1f5543f2e30144105
reozmen <recepozmen41@gmail.com>
2023-05-03 22:56:32 +0300

    .

--- Day_02/Exercises.js
+++ Day_02/Exercises.js
@@ -1,6 +1,19 @@
+//^^ Exercise 1:
 //^^ Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
 
 //^^ Challenge adında bir değişken tanımlayın ve bunu '30 Days Of JavaScript' başlangıç ​​değerine atayın.
 
 let challenge = "30 Days Of JavaScript";
 console.log(challenge);
+
+//^^ Exercise 2:
+//^^ Print the string on the browser console using console.log()
+//^^ console.log () kullanarak dizeyi tarayıcı konsoluna yazdırın
+
+console.log(challenge);
+
+//^^ Exercise 3:
+//^^ Print the length of the string on the browser console using console.log()
+//^^ console.log () kullanarak dizenin uzunluğunu tarayıcı konsoluna yazdırın
+
+console.log(challenge.length);
