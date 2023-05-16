76930a4ebe778c80173a9156c83a289ad3f73525
reozmen <recepozmen41@gmail.com>
2023-05-16 23:48:43 +0300

    .

--- booleanOperators.js
+++ booleanOperators.js
@@ -232,5 +232,6 @@ console.log(!(num39 == num40 && num39 === num40)); //* false
 
 console.log(!true); //* false
 console.log(!false); //* true
+console.log(!0); //* true
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!/
