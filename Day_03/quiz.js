fdce77fc45e7e30c0ba06df8594655e67f02e12a
reozmen <recepozmen41@gmail.com>
2023-05-19 21:50:03 +0300

    quiz started

---
+++ Day_03/quiz.js
@@ -0,0 +1,27 @@
+//^^ LEVEL 1    ^^
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//* 1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+
+//* 1.firstName, lastName, country, city, age, isMarried, year değişkenini bildirin ve ona bir değer atayın ve farklı veri türlerini kontrol etmek için typeof operatörünü kullanın.
+
+let firstName = "Mehmet";
+let lastName = "Kara";
+let country = "Turkey";
+let city = "Istanbul";
+let age = 25;
+let isMarried = false;
+let year = 2021;
+
+console.log(
+  "firstName : ",
+  firstName,
+  "=> tyypeof firstName : ",
+  typeof firstName
+);
+console.log(lastName, "=> tyypeof lastName : ", typeof lastName);
+console.log(country, "=> tyypeof country : ", typeof country);
+console.log(city, "=> tyypeof city : ", typeof city);
+console.log(age, "=> tyypeof age : ", typeof age);
+console.log(isMarried, "=> tyypeof isMarried : ", typeof isMarried);
+console.log(year, "=> tyypeof year : ", typeof year);
