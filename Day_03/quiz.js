c23f33bb2732e8fb1bd79cf6702fe4afd5c28dd2
reozmen <recepozmen41@gmail.com>
2023-05-19 22:03:47 +0300

    .

--- Day_03/quiz.js
+++ Day_03/quiz.js
@@ -64,3 +64,30 @@ console.log("1 === 1 : ", 1 === 1);
 console.log("false : ", false);
 console.log("1 == 2 : ", 1 == 2);
 console.log("1 === 2 : ", 1 === 2);
+
+//* Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//* Aşağıdaki karşılaştırma ifadesinin sonucunu öncelikle console.log() kullanmadan bulun. Sonuca karar verdikten sonra bunu console.log() kullanarak onaylayın.
+
+//* 4 > 3
+//* 4 >= 3
+//* 4 < 3
+//* 4 <= 3
+//* 4 == 4
+//* 4 === 4
+//* 4 != 4
+//* 4 !== 4
+//* 4 != '4'
+//* 4 == '4'
+//* 4 === '4'
+
+console.log("4 > 3 : ", 4 > 3);
+console.log("4 >= 3 : ", 4 >= 3);
+console.log("4 < 3 : ", 4 < 3);
+console.log("4 <= 3 : ", 4 <= 3);
+console.log("4 == 4 : ", 4 == 4);
+console.log("4 === 4 : ", 4 === 4);
+console.log("4 != 4 : ", 4 != 4);
+console.log("4 !== 4 : ", 4 !== 4);
+console.log("4 != '4' : ", 4 != "4");
+console.log("4 == '4' : ", 4 == "4");
+console.log("4 === '4' : ", 4 === "4");
